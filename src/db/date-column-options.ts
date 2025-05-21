fa2621c56f1f3fd70508afd35671c80899b10402
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2025-05-21 09:28:02 +0200

    fix: date transformer

--- src/db/date-column-options.ts
+++ src/db/date-column-options.ts
@@ -3,12 +3,10 @@ import { ColumnOptions } from "typeorm";
 export const DateColumnOptions: ColumnOptions = {
 	// sqlite does not support timestamptz column type
 	type: "text",
-	// by default typeorm uses YYYY-MM-DD HH:mm:ss which is inferior to ISO 8601
-	default: new Date().toISOString(),
 	transformer: {
 		// when saving to the database, we don't care about the type
 		to: (val: unknown) => val,
 		// when reading from the database, we want to convert the string to a date
-		from: (value: string): Date => new Date(value),
+		from: (value: string) => new Date(value),
 	},
 } as const;
